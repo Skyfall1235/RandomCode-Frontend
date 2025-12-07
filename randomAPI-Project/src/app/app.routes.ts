30e0958bdced1c8fa3dac458ef022e59b5cc9cfe
Skyfall1235 <113722636+Skyfall1235@users.noreply.github.com>
2025-12-07 16:13:04 -0500

    basic landing page

--- randomAPI-Project/src/app/app.routes.ts
+++ randomAPI-Project/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Landing } from './landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing }, // Our landing page is the home page
+  // { path: '**', redirectTo: '' } // Optional: Redirect any unknown path to home
+];
--- randomAPI-Project/src/app/landing/landing.ts
+++ randomAPI-Project/src/app/landing/landing.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-landing',
-  imports: [],
-  templateUrl: './landing.html',
-  styleUrl: './landing.css',
+  selector: 'app-landing', 
+  standalone: true,
+  imports: [CommonModule], 
+  templateUrl: './landing.html', 
+  styleUrl: './landing.css'
 })
 export class Landing {
 
